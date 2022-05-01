a04da3c2ad6df9c930316382ab2736737c44f18f
Shankar <89457569+Shankarprince@users.noreply.github.com>
2022-05-01 11:55:32 +0530

    committed

---
+++ Brands, Products API.js
@@ -0,0 +1,88 @@
+const products =
+    [
+        {
+            "id": 1,
+            "name": "iPhone 13 Pro Max",
+            "image": "https://rukminim1.flixcart.com/image/312/312/ktketu80/mobile/3/e/o/iphone-13-pro-max-mll63hn-a-apple-original-imag6vpgwfgxdsj6.jpeg?q=70",
+            "brand": "Apple",
+            "price": 179999,
+            "release": "14-Sep-2021"
+        },
+        {
+            "id": 2,
+            "name": "Samsung Galaxy Z Fold3 5G",
+            "image": "https://rukminim1.flixcart.com/image/416/416/ksqeky80/mobile/x/7/1/galaxy-z-fold3-5g-sm-f926bzgdinu-samsung-original-imag68q6hpdwmngw.jpeg?q=70",
+            "brand": "Samsung",
+            "price": 149999,
+            "release": "11-Aug-2021"
+        },
+        {
+            "id": 3,
+            "name": "Oneplus 9 Pro",
+            "image": "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ9ARLO2oWo6Ldm9XFZadzFxxWYZS_Oqv1dXmnPqSvXrjgbDB8Z51OxmuP939vOE5eS8Bd5AerGEoEY-sGq0En162TPxGc_IQ",
+            "brand": "Oneplus",
+            "price": 64999,
+            "release": "23-Mar-2021"
+        },
+        {
+            "id": 4,
+            "name": "Mi 10T Pro",
+            "image": "https://rukminim1.flixcart.com/image/312/312/kg6vfrk0/mobile/e/s/6/mi-10t-pro-mzb07zdin-original-imafwhbrythgcddt.jpeg?q=70",
+            "brand": "Xiaomi",
+            "price": 47999,
+            "release": "30-Sep-2020"
+        },
+        {
+            "id": 5,
+            "name": "Vivo X70 Pro+",
+            "image": "https://rukminim1.flixcart.com/image/312/312/ku4ezrk0/mobile/p/c/q/x70-pro-v2114-vivo-original-imag7bq5m6bbefup.jpeg?q=70",
+            "brand": "Vivo",
+            "price": 79990,
+            "release": "09-Sep-2021"
+        }
+    ];
+
+
+const brands =
+    [
+        {
+            "id": "1",
+            "name": "Apple"
+        },
+        {
+            "id": "1",
+            "name": "Samsung"
+        },
+        {
+            "id": "2",
+            "name": "Oneplus"
+        },
+        {
+            "id": "3",
+            "name": "Xiaomi"
+        },
+        {
+            "id": "4",
+            "name": "Vivo"
+        },
+        {
+            "id": "5",
+            "name": "Oppo"
+        },
+        {
+            "id": "6",
+            "name": "Moto"
+        },
+        {
+            "id": "7",
+            "name": "Micromax"
+        },
+        {
+            "id": "8",
+            "name": "Poco"
+        },
+        {
+            "id": "9",
+            "name": "IQOO"
+        }
+    ]
